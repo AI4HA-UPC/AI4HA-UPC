f4f6a1308601cbedea4a4cd57bc038ed74de094a
bejar <bejar@cs.upc.edu>
2024-11-06 12:52:40 +0100

    commit

---
+++ Polipos/ai4ha/paths.py
@@ -0,0 +1,18 @@
+LOCAL_REMOTE_PATHS_CONFIG = {
+    "Experiment": {
+        "local_path": "/home/bejar/bsc",
+        "remote_path": "/gpfs/projects/bsc70/bsc70642"
+    },
+    "Data": {
+        "local_path": "/home/bejar/bsc/Data",
+        "remote_path": "/data"
+    },
+    "Models": {
+        "local_path": "/home/bejar/bsc/Models",
+        "remote_path": "/models"
+    },
+    'Loss':
+        {
+            'root': '/gpfs/projects/bsc70/bsc70642/misiones/ai4ha/',
+        },
+}
